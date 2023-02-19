c0119b88a1df9e02e1e5e3e61b52a1b7d5aaf334
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-02-19 11:25:25 +0000

    Set up flask templates and vies and add views file

--- server.py
+++ server.py
@@ -1,44 +1,56 @@
+from flask import Flask
 import socket
 import threading
 import datetime
 
-HEADER = 64
-PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
-FORMAT = 'UTF-8'
-DISCONNECT_MESSAGE = '!DISCONNECT'
-DATE = datetime.datetime.today()
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((SERVER, PORT))
+app = Flask(__name__)
 
-def handle_client(conn, addr):
-    print(f'[New CONNECTION] {addr} connected' )
-
-    connected = True
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-
-        if msg_length:
-            msg_length =int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-
-            print(f'[{addr}] {msg}')
-
-    conn.close()
+@app.route('/')
+def home_page():
+    return 'Hello'
 
 
-def start_server():
-    server.listen()
-    print(f'[Sent Date : {DATE}] Server is listening on {SERVER} ')
-    while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        print(f'[ACTIVE CONNECTIONS] {threading.activeCount() - 1}')
+# HEADER = 64
+# PORT = 5050
+# SERVER = socket.gethostbyname(socket.gethostname())
+# FORMAT = 'UTF-8'
+# DISCONNECT_MESSAGE = '!DISCONNECT'
+# DATE = datetime.datetime.today()
+
+# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server.bind((SERVER, PORT))
+
+# def handle_client(conn, addr):
+#     print(f'[New CONNECTION] {addr} connected' )
+
+#     connected = True
+#     while connected:
+#         msg_length = conn.recv(HEADER).decode(FORMAT)
+
+#         if msg_length:
+#             msg_length =int(msg_length)
+#             msg = conn.recv(msg_length).decode(FORMAT)
+
+#             if msg == DISCONNECT_MESSAGE:
+#                 connected = False
+
+#             print(f'[{addr}] {msg}')
+
+#     conn.close()
 
 
-start_server()
+# def start_server():
+#     server.listen()
+#     print(f'[Sent Date : {DATE}] Server is listening on {SERVER} ')
+#     while True:
+#         conn, addr = server.accept()
+#         thread = threading.Thread(target=handle_client, args=(conn, addr))
+#         thread.start()
+#         print(f'[ACTIVE CONNECTIONS] {threading.activeCount() - 1}')
+
+
+# start_server()
+
+if __name__ == "__main__":
+    app.run(debug=True)
